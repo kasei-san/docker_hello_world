71175192630cd397cbeaae0eefc10462063222e7
kasei_san <kasei.san@gmail.com>
2020-02-04 11:20:20 +0900

    curlの処理が終了するまでwaitする

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl https://0ym4rbvub6.execute-api.us-east-1.amazonaws.com/production/create?branch_name=${BRANCH_NAME}&commit_hash=${CODEBUILD_RESOLVED_SOURCE_VERSION}
+curl https://0ym4rbvub6.execute-api.us-east-1.amazonaws.com/production/create?branch_name=${BRANCH_NAME}&commit_hash=${CODEBUILD_RESOLVED_SOURCE_VERSION} &
+pid=($!)
+wait ${pid}
