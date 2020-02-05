844abab75d41fec24651a8b9d674ea996dc6b175
kasei_san <kasei.san@gmail.com>
2020-02-05 16:32:55 +0900

    Remove test.sh

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl https://0ym4rbvub6.execute-api.us-east-1.amazonaws.com/production/create?branch_name=${BRANCH_NAME}&commit_hash=${CODEBUILD_RESOLVED_SOURCE_VERSION} &
-pid=($!)
-wait ${pid}
